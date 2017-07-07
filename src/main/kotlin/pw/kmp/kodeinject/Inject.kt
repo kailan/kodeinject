cf5ebceb9ceedebb08243b96ff1152a592d944de
Kailan Blanks <kailan@kmp.pw>
2017-07-07 20:24:21 +0100

    [1.2.0] Update to kodein 4.0.0

--- src/main/kotlin/pw/kmp/kodeinject/Inject.kt
+++ src/main/kotlin/pw/kmp/kodeinject/Inject.kt
@@ -1,14 +1,16 @@
+@file:Suppress("unused")
+
 package pw.kmp.kodeinject
 
 import com.github.salomonbrys.kodein.Kodein
-import com.github.salomonbrys.kodein.typeClass
+import com.github.salomonbrys.kodein.generic
 
 /**
  * Creates an injection provider: each time an instance is needed, the type will be instantiated.
  */
-inline fun <reified T : Any> Kodein.Builder.injected() = InjectionProvider<T>(typeClass<T>())
+inline fun <reified T : Any> Kodein.Builder.injected() = InjectionProvider(T::class, generic())
 
 /**
  * Creates an injection provider: the type will only be instantiated the first time it is needed.
  */
-inline fun <reified T : Any> Kodein.Builder.injectedSingleton() = SingletonInjectionProvider<T>(typeClass<T>())
+inline fun <reified T : Any> Kodein.Builder.injectedSingleton() = SingletonInjectionProvider(T::class, generic())
--- src/main/kotlin/pw/kmp/kodeinject/InjectionProvider.kt
+++ src/main/kotlin/pw/kmp/kodeinject/InjectionProvider.kt
@@ -1,40 +1,54 @@
 package pw.kmp.kodeinject
 
-import com.github.salomonbrys.kodein.*
-import java.lang.reflect.Type
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.TT
+import com.github.salomonbrys.kodein.TypeToken
+import com.github.salomonbrys.kodein.bindings.NoArgBinding
+import com.github.salomonbrys.kodein.bindings.NoArgBindingKodein
+import com.github.salomonbrys.kodein.description
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.jvm.javaType
 
 /**
  * Provides a factory that performs dependency injection into constructors.
  */
-open class InjectionProvider<out T : Any>(override val createdType: Type, val name: String = "injected") : Provider<T> {
+open class InjectionProvider<T : Any>(val type: KClass<T>, final override val createdType: TypeToken<T>) : NoArgBinding<T> {
 
-    override fun getInstance(kodein: ProviderKodein, key: Kodein.Key): T {
-        if (createdType !is Class<*>) throw IllegalArgumentException("Kodein can only instantiate classes.")
-        val clazz = createdType as Class<*>
-        val constructor = clazz.constructors
-                .filter { it.parameterTypes.all { t -> kodein.container.bindings.keys.any { it.bind.type == t } } }
-                .firstOrNull() ?: throw IllegalArgumentException("No constructors of ${clazz.name} match available bindings.\nRegistered in Kodein:\n" + kodein.container.bindings.description)
-        val args = constructor.parameterTypes.map { kodein.container.nonNullProvider(Kodein.Bind(it, null)).invoke() }
-        return constructor.newInstance(*args.toTypedArray()) as T
+    override fun getInstance(kodein: NoArgBindingKodein, key: Kodein.Key<Unit, T>): T {
+        val constructor = findConstructor(kodein)
+            ?: throw IllegalArgumentException("No constructors of ${type.simpleName} match available bindings.\nRegistered in Kodein:\n" + kodein.container.bindings.description)
+        val parameters = provideDependencies(constructor, kodein)
+        return constructor.call(*parameters)
     }
 
-    override fun factoryName() = name
-    override val description: String get() = "${factoryName()}<${createdType.simpleDispString}>()"
-    override val fullDescription: String get() = "${factoryName()}<${createdType.fullDispString}>()"
+    internal fun findConstructor(kodein: NoArgBindingKodein): KFunction<T>? {
+        return type.constructors.filter { it.parameters.all { p -> kodein.ProviderOrNull(TT(p.type.javaType as Class<*>)) != null } }.firstOrNull()
+    }
+
+    internal fun provideDependencies(constructor: KFunction<T>, kodein: NoArgBindingKodein): Array<Any?> {
+        return constructor.parameters.map { kodein.Provider(TT(it.type.javaType as Class<*>)).invoke() }.toTypedArray()
+    }
+
+    override fun factoryName() = "injected"
+    override val description: String get() = "${factoryName()}<${createdType.simpleDispString()}>()"
+    override val fullDescription: String get() = "${factoryName()}<${createdType.fullDispString()}>()"
 
 }
 
 /**
  * An injection provider that only creates a single instance of the type.
  */
-class SingletonInjectionProvider<T : Any>(createdType: Type) : InjectionProvider<T>(createdType, "injectedSingleton") {
+class SingletonInjectionProvider<T : Any>(type: KClass<T>, createdType: TypeToken<T>) : InjectionProvider<T>(type, createdType) {
 
     var instance: T? = null
 
-    override fun getInstance(kodein: ProviderKodein, key: Kodein.Key): T {
+    override fun getInstance(kodein: NoArgBindingKodein, key: Kodein.Key<Unit, T>): T {
         if (instance != null) return instance!!
         instance = super.getInstance(kodein, key)
         return instance!!
     }
+
+    override fun factoryName() = "injectedSingleton"
 
 }
